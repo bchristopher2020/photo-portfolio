89145961b9c4d4ca47fe7f85014bbfe286bfd4f9
bchristopher2020 <cwadeburt@gmail.com>
2020-12-03 13:21:18 -0600

    Major modal changes made

--- app.js
+++ app.js
@@ -20,3 +20,28 @@ links.forEach(link => {
     })
 
 })
+
+var modal = document.getElementById('myModal');
+var i;
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+// 
+for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+
+    modal.style.display = "block";
+       modalImg.src = this.src;
+
+ }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
