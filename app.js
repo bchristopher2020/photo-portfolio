6e90ba9fbeeb46ddd56bac999b3b3b8d5143b343
bchristopher2020 <cwadeburt@gmail.com>
2020-12-11 19:00:31 -0600

    removed carousel code, but added images so will merge

--- app.js
+++ app.js
@@ -32,29 +32,7 @@ var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-// Slideshow logic
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
-
-// end of slideshow logic
 
 for(i=0;i< img.length;i++)
    {    
