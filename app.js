9690532d6496fc1f985fd6a6fe3b941498420f58
bchristopher2020 <cwadeburt@gmail.com>
2020-12-11 18:05:34 -0600

    working on adding an image arousel

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const links = nav.querySelectorAll("a")
 
 
 
+
+
+
+
 burger.addEventListener("click", () => {
     nav.classList.toggle("nav-open")
     burger.classList.toggle("toggle")
@@ -27,7 +31,31 @@ var i;
 var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-// 
+
+// Slideshow logic
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// end of slideshow logic
+
 for(i=0;i< img.length;i++)
    {    
     img[i].onclick = function(){
